e62cad17c718afb350f8a8ed44cb6d5fe651c8e1
Egioooo <96048066+sergiomonts@users.noreply.github.com>
2023-01-31 21:21:52 +0100

    Update DatabaseSeeder.php

--- seeders/DatabaseSeeder.php
+++ seeders/DatabaseSeeder.php
@@ -14,17 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      //  $this->call(CompanySeeder::class);
+      //  $this->call(BudgetSeeder::class);
+      //  $this->call(TypeDeviceSeeder::class);
         $this->call(QuestionnaireSeeder::class);
-        $this->call(QuestionSeeder::class);
-        $this->call(ImpactSeeder::class);
-        $this->call(QuestionQuestionnaireSeeder::class);
-        $this->call(TypeMeasureSeeder::class);
+     //   $this->call(DeviceSeeder::class);
         $this->call(InterventionSeeder::class);
         $this->call(ProbabilitySeeder::class);
         $this->call(RiskSeeder::class);
+        $this->call(ImpactSeeder::class);
         $this->call(ReportSeeder::class);
+      //  $this->call(CourseSeeder::class);
+     //   $this->call(UserSeeder::class);
+        $this->call(TypeMeasureSeeder::class);
+       // $this->call(InventorySeeder::class);
+       // $this->call(EmblemSeeder::class);
+      //  $this->call(CategorySeeder::class);
+      //  $this->call(ResourceFileSeeder::class);
+       // $this->call(ResourceUrlSeeder::class);
+      //  $this->call(ResourceTextSeeder::class);
+      //  $this->call(ActivitySeeder::class);
+      //  $this->call(DeliverySeeder::class);
+      //  $this->call(GradeSeeder::class);
+      //  $this->call(RatingSeeder::class);
+      //  $this->call(CourseUserSeeder::class);
+      //  $this->call(QuestionnaireUserSeeder::class);
+        $this->call(QuestionSeeder::class);
+        $this->call(QuestionQuestionnaireSeeder::class);
         $this->call(AnswerSeeder::class);
         $this->call(ResultSeeder::class);
-  //      $this->call(QuestionnaireUserSeeder::class);
+     //   $this->call(TaskSeeder::class);
     }
 }
