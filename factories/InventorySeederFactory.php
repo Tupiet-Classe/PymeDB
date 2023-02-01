d10d2877efea95209e74dba2d74ada15ed670607
Tupiet <62516852+Tupiet@users.noreply.github.com>
2023-02-01 19:30:10 +0100

    Add files via upload

---
+++ factories/InventorySeederFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Device;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class InventorySeederFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => function () {
+                return User::inRandomOrder()->first()->id;
+            },
+            'device_id' => function () {
+                return Device::inRandomOrder()->first()->id;
+            },
+        ];
+    }
+}
