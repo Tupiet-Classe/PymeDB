422a2a0eecb8102156f76b8203e44cd0c1750502
samuel-lara <97664125+samuel-lara@users.noreply.github.com>
2023-02-02 08:52:14 +0100

    Update Grade.php

--- models/Grade.php
+++ models/Grade.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Grade extends Model
 {
     use HasFactory;
+    /** QUE ES ESTO TUDOR? SI LO PONEMOS DA FALLO */
     // Nom de la taula
-    protected $table = 'deliveries';
+    //protected $table = 'deliveries';
 
     // Camps de la taula a replenar (El id i el timespace no es fiquen)
     protected $fillable = [
