e74b244723608414c5920935949f7ab9f7df63b7
Tupiet <62516852+Tupiet@users.noreply.github.com>
2023-02-06 17:37:50 +0100

    Error a una taula solucionat

--- migrations/2014_01_01_000049_create_resource_urls_table.php
+++ migrations/2014_01_01_000049_create_resource_urls_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('resource_files', function (Blueprint $table) {
+        Schema::create('resource_urls', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->nullable(false);
             $table->string('location', 255)->nullable(false);
