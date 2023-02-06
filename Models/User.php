dbab39c89d5fb25df9563b2fefca2c1cd92d31d9
samuel-lara <97664125+samuel-lara@users.noreply.github.com>
2023-02-06 21:06:57 +0100

    Update User.php

--- Models/User.php
+++ Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Device::class);
     }
+
+    public function reports()
+    {
+        return $this->hasToMany(Report::class);
+    }
 }
