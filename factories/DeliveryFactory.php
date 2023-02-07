56dd87543b77e264aa6a0aca42e56d0a2020841e
Tud0r420 <94829026+Tud0r420@users.noreply.github.com>
2023-02-07 19:29:47 +0100

    Update DeliveryFactory.php

--- factories/DeliveryFactory.php
+++ factories/DeliveryFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use app\Models\User;
 use app\Models\Activity;
+use Illuminate\Support\Str;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Delivery>
  */
@@ -19,11 +21,20 @@ class DeliveryFactory extends Factory
     {
         $activity_id = Activity::all()->pluck('id')->toArray();
         $user_id = User::all()->pluck('id')->toArray();
+
+        $file_name = Str::random(10) . ".txt";
+        $file_path = storage_path("app/public/files/{$file_name}");
+        $file = fopen($file_path, "w");
+        $random_string = $this->faker->paragraph;
+        fwrite($file, $random_string);
+        fclose($file);
+
         return [
-            'locate'=>$this->faker->city(),
-            'grade'=>$this->faker->numberBetween(0,1),
-            'activity_id'=>$this->faker->randomElement($activity_id),
-            'user_id'=>$this->faker->randomElement($user_id),
+            'locate' =>  $file_path,
+            'grade' => $this->faker->numberBetween(0, 1),
+            'feedback' => $this->faker->text(),
+            'activity_id' => $this->faker->randomElement($activity_id),
+            'user_id' => $this->faker->randomElement($user_id),
 
         ];
     }
