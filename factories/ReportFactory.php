4f67dc3e1a008fd7b8443f4b5e042fe6fa92fdb5
francisco-zr <94826092+francisco-zr@users.noreply.github.com>
2023-02-27 17:28:08 +0100

    Update ReportFactory.php

--- factories/ReportFactory.php
+++ factories/ReportFactory.php
@@ -25,6 +25,7 @@ class ReportFactory extends Factory
 
         return [
             'name' => $this->faker->unique()->name(),
+            'status' => fake()->randomElement(['done', 'pending']),
             'user_id' => $this->faker->randomElement($user_id),
             'questionnaire_id' => $this->faker->randomElement($questionnaire_id),
             'date' => $this->faker->date(),
