82c5dcbe80511f6ce1e11a7c3fee052564c48811
Fran Zahinos <franciscozahinos@iesmontsia.org>
2023-03-07 20:21:30 +0100

    Revert "Update QuestionSeeder.php"
    This reverts commit 9a171e2bc3589dd29150e49895e404131d983864.

--- seeders/QuestionSeeder.php
+++ seeders/QuestionSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Answer;
 use App\Models\Question;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,20 +15,6 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-
-        // Creo 40 preguntas con 2 respuestas cada una
-        for ($i = 1; $i <= 40; $i++) {
-            $question = Question::factory()->create();
-
-            Answer::factory()->create([
-                'question_id' => $question->id,
-                'name' => 'Sí',
-            ]);
-
-            Answer::factory()->create([
-                'question_id' => $question->id,
-                'name' => 'No',
-            ]);
-        }
+        Question::factory(12)->create();
     }
 }
