089cf0b97b47c17accca8c23779bae44810867c3
Tupiet <62516852+Tupiet@users.noreply.github.com>
2023-03-14 16:28:26 +0100

    Update 2014_01_01_000012_create_devices_table.php

--- migrations/2014_01_01_000012_create_devices_table.php
+++ migrations/2014_01_01_000012_create_devices_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('mac_ethernet')->nullable();
             $table->string('mac_wifi')->nullable();
             $table->text('description')->nullable();
-            $table->string('state')->nullable();
+            $table->enum('state', ['Operativo', 'En mantenimiento', 'Deshabilitado'])->nullable();
             $table->string('tag')->nullable();
             $table->string('serial_number');
             $table->foreignId('type_device_id')->constrained();
