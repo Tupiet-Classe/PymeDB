711125b56325064945e74a37c32692997341f098
ivansorribes <92729516+ivansorribes@users.noreply.github.com>
2023-03-29 17:49:40 +0200

    Update 2014_01_01_000030_create_courses_table.php

--- migrations/2014_01_01_000030_create_courses_table.php
+++ migrations/2014_01_01_000030_create_courses_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50)->nullable();
-            $table->string('description', 50);
+            $table->string('description', 255);
             $table->longText('image')->nullable();
             $table->date('hidden')->nullable()->default(null);
             $table->timestamps();
